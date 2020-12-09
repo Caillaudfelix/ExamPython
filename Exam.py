e44cfc4f83f0203f8d6936562b3359206f753473
Caillaudfelix <74654899+Caillaudfelix@users.noreply.github.com>
2020-12-09 12:03:56 +0100

    tuez moi

--- Exam.py
+++ Exam.py
@@ -14,8 +14,27 @@ from colorama import Fore, Back
 mot_mystere=mot5
 tour=1
 tableau_base=["_","_","_","_","_","_"]
-tableau_mm=["renard"]
-tableau_joueur=[""]
+tableau_mm=["r","e","n","a","r","d",]
+tableau_joueur=[" "]
+
+def perdu(tableau_joueur,tableau_mm):
+    for i in range(0,7):
+        if(tour==8):
+            print("Vous avez perdu !")
+    return
+
+def victoire(tableau_joueur,tableau_mm):
+    for i in range(0,7):
+        if(tableau_joueur[1-7] == tableau_mm[1] and tableau_mm[2] and tableau_mm[3] and tableau_mm[4] and tableau_mm[5] and tableau_mm[6]):
+            print("Vous avez gagné !")
+    return
+
+def recommencer(tableau_joueur, tableau_mm):
+	for i in range(0,7):
+		if(tableau_joueur[1-7] != tableau_mm[1] or tableau_mm[2] or tableau_mm[3] or tableau_mm[4] or tableau_mm[5] or tableau_mm[6]):
+			print("Votre mot ne correspond pas au mot mystère !")
+	return
+
 print(tableau_base)
 guess=input("Quel est votre choix ?")
 for x in range(1):
@@ -30,12 +49,4 @@ for x in range(1):
         tour+=1
         continue
 
-def perdu(guess):
-    if(tour==8) and (guess!=mot_mystere):
-        print("Vous avez perdu !")
-    return
-
-def victoire(guess):
-    if(guess==mot_mystere):
-        print("Vous avez gagné !")
-    return
+input()

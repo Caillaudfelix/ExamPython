0e1782d109ec53722f9573bebc450633c60b1dc6
Caillaudfelix <74654899+Caillaudfelix@users.noreply.github.com>
2020-12-09 12:32:01 +0100

    test mauvaise lettre

--- Exam.py
+++ Exam.py
@@ -18,22 +18,65 @@ tableau_mm=["r","e","n","a","r","d",]
 tableau_joueur=[" "]
 
 def perdu(tableau_joueur,tableau_mm):
-    for i in range(0,7):
+    for i in range(1,7):
         if(tour==8):
             print("Vous avez perdu !")
     return
 
 def victoire(tableau_joueur,tableau_mm):
-    for i in range(0,7):
+    for i in range(1,7):
         if(tableau_joueur[1] and tableau_joueur[2] and tableau_joueur[3] and tableau_joueur[4] and tableau_joueur[5] and tableau_joueur[6] == tableau_mm[1] and tableau_mm[2] and tableau_mm[3] and tableau_mm[4] and tableau_mm[5] and tableau_mm[6]):
             print("Vous avez gagné !")
     return
 
 def recommencer(tableau_joueur,tableau_mm):
-	for i in range(0,7):
+	for i in range(1,7):
 		if(tableau_joueur[1] or tableau_joueur[2] or tableau_joueur[3] or tableau_joueur[4] or tableau_joueur[5] or tableau_joueur[6] != tableau_mm[1] or tableau_mm[2] or tableau_mm[3] or tableau_mm[4] or tableau_mm[5] or tableau_mm[6]):
 			print("Votre mot ne correspond pas au mot mystère !")
 	return
+
+def mauvaiseLettre1(tableau_joueur,tableau_mm):
+	for i in range(1,7):
+		if(tableau_joueur[1] != tableau_mm[1]):
+			print(tableau_joueur)
+			print(back.BLUE+tableau_joueur[1])
+	return
+
+def mauvaiseLettre2(tableau_joueur,tableau_mm):
+	for i in range(1,7):
+		if(tableau_joueur[2] != tableau_mm[2]):
+			print(tableau_joueur)
+			print(back.BLUE+tableau_joueur[2])
+	return
+
+def mauvaiseLettre3(tableau_joueur,tableau_mm):
+	for i in range(1,7):
+		if(tableau_joueur[3] != tableau_mm[3]):
+			print(tableau_joueur)
+			print(back.BLUE+tableau_joueur[3])
+	return
+
+def mauvaiseLettre4(tableau_joueur,tableau_mm):
+	for i in range(1,7):
+		if(tableau_joueur[4] != tableau_mm[4]):
+			print(tableau_joueur)
+			print(back.BLUE+tableau_joueur[4])
+	return
+
+def mauvaiseLettre5(tableau_joueur,tableau_mm):
+	for i in range(1,7):
+		if(tableau_joueur[5] != tableau_mm[5]):
+			print(tableau_joueur)
+			print(back.BLUE+tableau_joueur[5])
+	return
+
+def mauvaiseLettre6(tableau_joueur,tableau_mm):
+	for i in range(1,7):
+		if(tableau_joueur[6] != tableau_mm[6]):
+			print(tableau_joueur)
+			print(back.BLUE+tableau_joueur[6])
+	return
+
 
 print(tableau_base)
 guess=input("Quel est votre choix ?")

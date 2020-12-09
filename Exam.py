063e66b5e3607c8420e6053b428018f1725ce020
Caillaudfelix <74654899+Caillaudfelix@users.noreply.github.com>
2020-12-09 11:09:20 +0100

    test bon mot 2

--- Exam.py
+++ Exam.py
@@ -8,32 +8,34 @@ mot7="citron"
 mot8="lettre"
 mot9="wagons"
 mot10="entrer"
+from colorama import init
+init()
+from colorama import Fore, Back
 mot_mystere=mot5
 tour=1
-tableau=[" "," "," "," "," "," "]
+tableau_base=["_","_","_","_","_","_"]
 tableau_mm=["renard"]
 tableau_joueur=[""]
-print(tableau)
+print(tableau_base)
 guess=input("Quel est votre choix ?")
 for x in range(1):
     nbrLettres=len(guess)
     if(nbrLettres!=6):
         print("Votre mot ne compte pas 6 lettres !")
-        tour=tour
         continue
     else:
         tableau_joueur[x]=guess
         print(tableau_joueur)
+        print(tableau_base)
+        tour+=1
         continue
 
-def perdu(tableau_joueur):
+def perdu(guess):
     if(tour==8) and (guess!=mot_mystere):
         print("Vous avez perdu !")
     return
 
-def victoire(tableau_joueur):
+def victoire(guess):
     if(guess==mot_mystere):
         print("Vous avez gagné !")
     return
-
-input()
